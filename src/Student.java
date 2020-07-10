58bda59fb323e5937db922728e4b6d10e7976807
swizz <swizze@bk.ru>
2020-07-10 10:33:19 +0300

    lesson twelve

--- src/Student.java
+++ src/Student.java
@@ -1,52 +1,41 @@
 import java.util.Set;
 
 public class Student {
-    public String name;
-    public String soname;
-    public int age;
-    public int groupID = 0;
-    public int problemSolution;
-    public int moduleSolution;
-    public boolean experience;
-    Student newStudent1 = new Student("vasya:", "knopkin", 22);
-    Student newStudent2 = new Student("alosha", "masterkin", 24);
-
-    public void setNewStudent1(Student newStudent1) {
-        experience = true;
-        moduleSolution = 5;
-        problemSolution = 6;
-    }
-
-    public void setNewStudent2(Student newStudent2) {
-        experience = false;
-        moduleSolution = 3;
-        problemSolution = 1;
-    }
+    private String name;
+    private String soname;
+    private int age;
+    private int groupID;
+    private String targetOfTraining;
+    private int problemSolution;
+    private int moduleSolution;
+    private boolean experience;
 
     public Student(String name, String soname, int age) {
+        this(name, soname, age, 0, 0, 0, false);
+    }
 
+    public Student(String name, String soname, int age, int groupID, int problemSolution, int moduleSolution, boolean experience) {
         this.name = name;
         this.soname = soname;
         this.age = age;
-    }
-
-    public Student(int groupID, int problemSolution, int moduleSolution, boolean experience) {
         this.groupID = groupID;
         this.problemSolution = problemSolution;
         this.moduleSolution = moduleSolution;
         this.experience = experience;
     }
+
     public int getGroupID(){
         return groupID;
     }
     public int getProblemSolution(){
         return problemSolution;
     }
+
     public int getModuleSolution(){
         return moduleSolution;
     }
+
     public boolean getExperience(){
         return experience;
     }
-
 }
